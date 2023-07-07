0ac683e9d9809314735a9eb224bbfdfa93830d7c
BricMichael <bricmichael@gmail.com>
2023-07-06 23:46:44 -0400

    Datos pago card | Clientes

--- src/components/CardRightDatosPagos/CardRightDatosPagos.js
+++ src/components/CardRightDatosPagos/CardRightDatosPagos.js
@@ -7,13 +7,67 @@ function CardRightDatosPagos() {
         <h4 className={style.title}>Datos del Pago</h4>
 
         <div className={style.contentTop}>
-            Datos Top
+            <div>
+                <p className={style.descripcion}>Fecha</p>
+                <p className={style.data}>Sabado, 24 Febrero 2023</p>
+            </div>
+            <p className={style.descripcion}>Numero de Referencia</p>
+            <p className={style.data}>0973</p>
+            <div className={style.horizontal}>
+                <div>
+                  <p className={style.descripcion}>Tipo de Plan</p>
+                  <p className={style.data}>Basico</p> 
+                </div>  
+                <div>
+                  <p className={style.descripcion}>Formato de Pago</p>
+                  <p className={style.data}>Transferencia</p> 
+                </div>           
+            </div>
+            <div className={style.horizontal}>
+                <div>
+                  <p className={style.descripcion}>N° de Meses que Pago</p>
+                  <p className={style.data}>4</p> 
+                </div>  
+                <div>
+                  <p className={style.descripcion}>N° de Meses a Deber</p>
+                  <p className={style.data}>9</p> 
+                </div>           
+            </div>
         </div>
         <div className={style.contentCenter}>
-            datos Center
+          <p className={style.titleCenter}>Monto y Pago</p>
+            <div className={style.mesesPagados}>
+                <div className={style.mesPagado}>
+                  <p className={style.mes}>Enero</p>
+                  <p className={style.monto}>250.00</p>
+                </div>
+                <div className={style.mesPagado}>
+                  <p className={style.mes}>Febrero</p>
+                  <p className={style.monto}>250.00</p>
+                </div>
+                <div className={style.mesPagado}>
+                  <p className={style.mes}>Marzo</p>
+                  <p className={style.monto}>250.00</p>
+                </div>
+                <div className={style.mesPagado}>
+                  <p className={style.mes}>Abril</p>
+                  <p className={style.monto}>250.00</p>
+                </div>
+            </div>
+            <div className={style.montoEnMoneda}>
+                <div className={`${style.mesPagado_center} ${style.monedaBs}`}>
+                  <p>Precio Total Bs</p>
+                  <p>750.00</p>
+                </div>
+                <div className={`${style.mesPagado_center} ${style.monedaDolar}`}>
+                  <p>Precio $</p>
+                  <p>32.10 $</p>
+                </div>
+            </div>
         </div>
         <div className={style.contentBottom}>
-            datos bottom
+            <button type='button' className={style.confirmBtn}>Confirmar</button>
+            <p className={style.cancelarBtn}>Cancelar</p>
         </div>
     </div>
   )
